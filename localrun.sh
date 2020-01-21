281b5b9ea5c11ad6807179a6a317dd62ce788c9e
James Chuang <chuang.ja@gmail.com>
2020-01-21 10:37:29 -0500

    Add localrun helper script.

---
+++ localrun.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+snakemake -pr \
+    -R `cat <(snakemake --lc --rerun-incomplete) \
+            <(snakemake --li --rerun-incomplete) \
+            <(snakemake --lp --rerun-incomplete) | sort -u` \
+    --rerun-incomplete \
+    --use-conda \
+    --cores
+
